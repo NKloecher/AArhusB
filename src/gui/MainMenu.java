3097f323a2f78b171b4f5fff0f6125031847f899
Mathias Hansen <mathiasosthan@gmail.com>
2017-03-30 14:57:22 +0200

    added main menu

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,17 +1,35 @@
 package gui;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class MainMenu extends GridPane {
+	private SelectHandler selectHandler;
+	private final Controller controller = new Controller();
+	
 	public MainMenu() {
-		this.add(this.getBigButton("Lav salg"), 0, 0);
-		this.add(this.getBigButton("Statestik"), 0, 1);
-		this.add(this.getBigButton("Udlejninger"), 0, 2);
-		this.add(this.getBigButton("Produkter"), 0, 3);
-		this.add(this.getBigButton("Prisliter"), 0, 4);
-		this.add(this.getBigButton("Brugere"), 0, 5);
+		Button sale = getBigButton("Lav salg");
+		add(sale, 0, 0);
+		
+		Button statistics = getBigButton("Statestik");
+		add(statistics, 0, 1);
+		
+		Button rentals = getBigButton("Udlejninger");
+		add(rentals, 0, 2);
+		
+		Button products = getBigButton("Produkter");
+		add(products, 0, 3);
+		
+		Button pricelists = getBigButton("Prislister");
+		add(pricelists, 0, 4);
+		
+		Button users = getBigButton("Brugere");
+		users.setOnAction(e -> controller.select(new Users()));
+		add(users, 0, 5);
+	}
+	
+	public void setOnSelect(SelectHandler handler) {
+		selectHandler = handler;
 	}
 	
 	private Button getBigButton(String text) {
@@ -22,5 +40,13 @@ public class MainMenu extends GridPane {
 		b.setPrefHeight(170);
 		
 		return b;
+	}
+	
+	private class Controller {
+		private void select(GridPane pane) {
+			if (selectHandler == null) return;
+			
+			selectHandler.exec(pane);
+		}
 	}
 }
