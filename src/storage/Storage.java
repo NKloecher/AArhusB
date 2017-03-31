ec3b46b6562d8cc0d60e857a4f18dd4a962db6d5
Nicolai K <eaaniklo@students.eaaa.dk>
2017-03-31 08:49:11 +0200

    smårettelser

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -6,55 +6,80 @@ import java.util.List;
 import model.*;
 
 public class Storage {
-	private final static Storage instance = new Storage();
-	
-	private final List<User> users = new ArrayList<>();
-	private final List<Product> products = new ArrayList<>();
-	private final List<Pricelist> pricelists = new ArrayList<>();
-	private final List<Payment> payments = new ArrayList<>();
-	private final List<Order> orders = new ArrayList<>();
+    private final static Storage instance = new Storage();
 
-	private Storage() {}
-	
-	public List<User> getUsers() {
-		return new ArrayList<>(users);
-	}
-	public void addUser(User u) {
-		users.add(u);
-	}
-	public void deleteUser(User u) {
-		users.remove(u);
-	}
-	
-	public static Storage getInstance() {
-		return instance;
-	}
+    private final List<User> users = new ArrayList<>();
+    private final List<Product> products = new ArrayList<>();
+    private final List<Pricelist> pricelists = new ArrayList<>();
+    private final List<Payment> payments = new ArrayList<>();
+    private final List<Order> orders = new ArrayList<>();
+    private final List<Tour> tours = new ArrayList<>();
+    private final List<Customer> customers = new ArrayList<>();
 
-	public List<Product> getProduct() {
-		return new ArrayList<>(products);
-	}
-	public void addProduct(Product p) {
-		products.add(p);
-	}
+    private Storage() {
+    }
 
-	public List<Pricelist> getPricelists() {
-		return new ArrayList<>(pricelists);
-	}
-	public void addPricelist(Pricelist pricelist) {
-		pricelists.add(pricelist);
-	}
+    public static Storage getInstance() {
+        return instance;
+    }
 
-	public List<Payment> getPayments() {
-		return new ArrayList<>(payments);
-	}
-	public void addPayment(Payment payment) {
-		payments.add(payment);
-	}
+    public List<Tour> getTours() {
+        return new ArrayList<>(tours);
+    }
 
-	public List<Order> getOrders() {
-		return new ArrayList<>(orders);
-	}
-	public void addOrder(Order order) {
-		orders.add(order);
-	}
+    public void addTour(Tour tour) {
+        tours.add(tour);
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(customers);
+    }
+
+    public void addCustomer(Customer c) {
+        customers.add(c);
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>(users);
+    }
+
+    public void addUser(User u) {
+        users.add(u);
+    }
+
+    public void deleteUser(User u) {
+        users.remove(u);
+    }
+
+    public List<Product> getProduct() {
+        return new ArrayList<>(products);
+    }
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
+    public List<Pricelist> getPricelists() {
+        return new ArrayList<>(pricelists);
+    }
+
+    public void addPricelist(Pricelist pricelist) {
+        pricelists.add(pricelist);
+    }
+
+    public List<Payment> getPayments() {
+        return new ArrayList<>(payments);
+    }
+
+    public void addPayment(Payment payment) {
+        payments.add(payment);
+    }
+
+    public List<Order> getOrders() {
+        return new ArrayList<>(orders);
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
 }
