42f0521493df4c9fa1019d264f5316877b6c742a
Nicolai K <eaaniklo@students.eaaa.dk>
2017-03-31 10:51:56 +0200

    produkt liste

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -4,49 +4,56 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class MainMenu extends GridPane {
-	private SelectHandler selectHandler;
-	private final Controller controller = new Controller();
-	
-	public MainMenu() {
-		Button sale = getBigButton("Lav salg");
-		add(sale, 0, 0);
-		
-		Button statistics = getBigButton("Statestik");
-		add(statistics, 0, 1);
-		
-		Button rentals = getBigButton("Udlejninger");
-		add(rentals, 0, 2);
-		
-		Button products = getBigButton("Produkter");
-		add(products, 0, 3);
-		
-		Button pricelists = getBigButton("Prislister");
-		add(pricelists, 0, 4);
-		
-		Button users = getBigButton("Brugere");
-		users.setOnAction(e -> controller.select(new Users()));
-		add(users, 0, 5);
-	}
-	
-	public void setOnSelect(SelectHandler handler) {
-		selectHandler = handler;
-	}
-	
-	private Button getBigButton(String text) {
-		Button b = new Button(text);
-		
-		b.setStyle("-fx-font-size: 24px");
-		b.setPrefWidth(300);
-		b.setPrefHeight(170);
-		
-		return b;
-	}
-	
-	private class Controller {
-		private void select(GridPane pane) {
-			if (selectHandler == null) return;
-			
-			selectHandler.exec(pane);
-		}
-	}
+    private SelectHandler selectHandler;
+    private final Controller controller = new Controller();
+
+    public MainMenu() {
+        Button sale = getBigButton("Lav salg");
+        add(sale, 0, 0);
+
+        Button statistics = getBigButton("Statestik");
+        add(statistics, 0, 1);
+
+        Button rentals = getBigButton("Udlejninger");
+        add(rentals, 0, 2);
+
+        Button tours = getBigButton("Rundvisninger");
+        add(tours, 0, 3);
+
+        Button products = getBigButton("Produkter");
+        products.setOnAction(e -> controller.select(new Products()));
+        add(products, 0, 4);
+
+        Button pricelists = getBigButton("Prislister");
+        add(pricelists, 0, 5);
+
+        Button users = getBigButton("Brugere");
+        users.setOnAction(e -> controller.select(new Users()));
+        add(users, 0, 6);
+
+    }
+
+    public void setOnSelect(SelectHandler handler) {
+        selectHandler = handler;
+    }
+
+    private Button getBigButton(String text) {
+        Button b = new Button(text);
+
+        b.setStyle("-fx-font-size: 24px");
+        b.setPrefWidth(300);
+        b.setPrefHeight(170);
+
+        return b;
+    }
+
+    private class Controller {
+        private void select(GridPane pane) {
+            if (selectHandler == null) {
+                return;
+            }
+
+            selectHandler.exec(pane);
+        }
+    }
 }
---
+++ src/gui/Products.java
@@ -0,0 +1,53 @@
+package gui;
+
+import gui.table.PrimitiveColumn;
+import gui.table.Table;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import model.Product;
+import service.Service;
+import storage.Storage;
+
+public class Products extends GridPane {
+
+    private final Storage storage = Storage.getInstance();
+    private final Service service = Service.getInstance();
+    private final Controller controller = new Controller();
+    private final Table<Product> table = new Table<>();
+
+    public Products() {
+
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(table);
+
+        table.addColumn(new PrimitiveColumn<Product, String>("Navn", x -> x.getName(),
+            (x, y) -> controller.updateName(x, y)));
+        table.addColumn(
+            new PrimitiveColumn<Product, String>("Kategori", x -> x.getCategory(),
+                (x, y) -> controller.updateCategori(x, y)));
+        table.setItems(storage.getProduct());
+        table.addColumn(new PrimitiveColumn<Product, Integer>("Klips", x -> x.getClips(),
+            (x, y) -> controller.updateClips(x, y)));
+
+        add(sp, 0, 0);
+
+    }
+
+    private class Controller {
+        public void updateName(Product product, String name) {
+            service.updateProductName(product, name);
+        }
+
+        public Object updateClips(Product x, String y) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public Object updateCategori(Product x, String y) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+
+}
