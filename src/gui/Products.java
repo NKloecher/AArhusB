f93534d6cf535fc3036d678c68c244a46f8135b5
Nicolai K <eaaniklo@students.eaaa.dk>
2017-03-31 12:49:10 +0200

    produktliste med kategori

--- src/gui/Products.java
+++ src/gui/Products.java
@@ -1,8 +1,12 @@
 package gui;
 
+import gui.table.ListColumn;
 import gui.table.PrimitiveColumn;
 import gui.table.Table;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.GridPane;
 import model.Product;
@@ -15,6 +19,7 @@ public class Products extends GridPane {
     private final Service service = Service.getInstance();
     private final Controller controller = new Controller();
     private final Table<Product> table = new Table<>();
+    private final TextField txfCategory = new TextField();
 
     public Products() {
 
@@ -25,20 +30,33 @@ public class Products extends GridPane {
 
         table.addColumn(new PrimitiveColumn<Product>("Navn", x -> x.getName(),
             (x, y) -> controller.updateName(x, y)));
-        table.addColumn(
-            new PrimitiveColumn<Product>("Kategori", x -> x.getCategory(),
-                (x, y) -> controller.updateCategori(x, y)));
+//        table.addColumn(
+//            new PrimitiveColumn<Product>("Kategori", x -> x.getCategory(),
+//                (x, y) -> controller.updateCategori(x, y)));
+        table.addColumn(new ListColumn<Product, String>("Kategori", x -> x.getCategory(),
+            (x, y) -> controller.updateCategory(x, y), storage.getCategories()));
         table.addColumn(new PrimitiveColumn<Product>("Klips", x -> x.getClips(),
             (x, y) -> controller.updateClips(x, y)));
 
         table.setItems(storage.getProduct());
         add(sp, 0, 0);
 
+        add(txfCategory, 1, 1);
+
+        Button btnCreateCategory = new Button("Lav ny Kategori");
+        add(btnCreateCategory, 2, 1);
+        btnCreateCategory.setOnAction(e -> controller.createCategoryAction());
+
     }
 
     private class Controller {
+
         public void updateName(Product product, String name) {
             service.updateProductName(product, name);
+        }
+
+        public void createCategoryAction() {
+            service.addCategory(txfCategory.getText().trim());
         }
 
         public void updateClips(Product product, String clips) {
@@ -51,7 +69,7 @@ public class Products extends GridPane {
 
         }
 
-        public void updateCategori(Product x, String category) {
+        public void updateCategory(Product x, String category) {
             service.updateProductCategory(x, category);
         }
 
