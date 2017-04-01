313127c226497c5c6309118aa5e7eb223de4d79d
mathias hansen <mathiasosthan@gmail.com>
2017-04-01 13:08:39 +0200

    added image to productlist

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -3,59 +3,61 @@ package gui;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class MainMenu extends GridPane {
-	private SelectHandler selectHandler;
+	private Handler<Pane> selectHandler;
 	private final Controller controller = new Controller();
-	
+
 	public MainMenu() {
 		setHgap(10);
 		setVgap(10);
 		setAlignment(Pos.TOP_CENTER);
-		
+
 		Button sale = getBigButton("Lav salg");
 		add(sale, 0, 0);
-		
+
 		Button statistics = getBigButton("Statestik");
 		add(statistics, 0, 1);
-		
+
 		Button rentals = getBigButton("Udlejninger");
 		add(rentals, 0, 2);
-		
+
 		Button tours = getBigButton("Rundvisninger");
-        add(tours, 0, 3);
+		add(tours, 0, 3);
 
-        Button products = getBigButton("Produkter");
-        products.setOnAction(e -> controller.select(new Products()));
-        add(products, 0, 4);
+		Button products = getBigButton("Produkter");
+		products.setOnAction(e -> controller.select(new Products()));
+		add(products, 0, 4);
 
-        Button pricelists = getBigButton("Prislister");
+		Button pricelists = getBigButton("Prislister");
 		add(pricelists, 0, 5);
 		pricelists.setOnAction(e -> controller.select(new Pricelists()));
-		
+
 		Button users = getBigButton("Brugere");
 		users.setOnAction(e -> controller.select(new Users()));
 		add(users, 0, 6);
 	}
-	
-	public void setOnSelect(SelectHandler handler) {
+
+	public void setOnSelect(Handler<Pane> handler) {
 		selectHandler = handler;
 	}
-	
+
 	private Button getBigButton(String text) {
 		Button b = new Button(text);
-		
+
 		b.setStyle("-fx-font-size: 24px");
 		b.setPrefWidth(300);
 		b.setPrefHeight(170);
-		
+
 		return b;
 	}
-	
+
 	private class Controller {
 		private void select(GridPane pane) {
-			if (selectHandler == null) return;
-			
+			if (selectHandler == null)
+				return;
+
 			selectHandler.exec(pane);
 		}
 	}
--- src/gui/ProductList.java
+++ src/gui/ProductList.java
@@ -1,14 +1,21 @@
 package gui;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.TextAlignment;
 import model.Pricelist;
 import model.Product;
 import service.Service;
@@ -21,48 +28,99 @@ public class ProductList extends BorderPane {
 	private final Pricelist pricelist = service.getSelectedPricelist();
 	private final TextField tfSearch = new TextField();
 	private final ComboBox<String> cbCategories = new ComboBox<>();
-	
-	public ProductList() {
+	private final List<Product> selectedProducts = new ArrayList<>();
+	private final Handler<Product> selectHandler;
+	private final Handler<Product> deselectHandler;
+
+	public ProductList(Handler<Product> selectHandler, Handler<Product> deselectHandler) {
+		this.selectHandler = selectHandler;
+		this.deselectHandler = deselectHandler;
+
+		setMaxWidth(460);
+
 		HBox hbQuery = new HBox();
-		
+		hbQuery.setStyle("-fx-padding: 0 0 16px 0;");
+
 		tfSearch.setOnKeyTyped(e -> controller.findProduct());
 		hbQuery.getChildren().add(tfSearch);
-		
-		List<String> categories = storage.getCategories();		
+
+		List<String> categories = storage.getCategories();
 		categories.add(0, "All");
 		cbCategories.getItems().setAll(categories);
 		cbCategories.valueProperty().addListener(e -> controller.findProduct());
 		cbCategories.getSelectionModel().select(0);
 		hbQuery.getChildren().add(cbCategories);
-		
+
 		controller.showProducts(pricelist.getProducts());
-		
+
 		setTop(hbQuery);
 	}
-	
+
 	class Controller {
 		public void findProduct() {
 			String selectedCategory = cbCategories.getSelectionModel().getSelectedItem();
 			String query = tfSearch.getText();
-			
-			List<Product> matchingProducts = service.getMatchingProducts(query, selectedCategory, pricelist.getProducts());
+
+			List<Product> matchingProducts = service.getMatchingProducts(query, selectedCategory,
+					pricelist.getProducts());
 			showProducts(matchingProducts);
 		}
-		
+
 		public void showProducts(List<Product> products) {
 			GridPane pane = new GridPane();
-			
+
 			pane.setHgap(10);
 			pane.setVgap(10);
 			pane.setAlignment(Pos.TOP_CENTER);
-			
+
 			for (int i = 0; i < products.size(); i++) {
 				Product p = products.get(i);
-				Label l = new Label(p.getName());
-				
-				pane.add(l, i%4, i/4);
+				Node node;
+
+				if (p.getImage() != null) {
+					ImageView image = new ImageView(
+							new Image("file:\\\\..\\product_images\\" + p.getImage(), 80, 80, true, true));
+					BorderPane bp = new BorderPane(image);
+
+					bp.setPadding(new Insets(10));
+
+					node = bp;
+				} else {
+					Label label = new Label(p.getName());
+
+					label.setMinWidth(100);
+					label.setMaxWidth(100);
+					label.setMinHeight(100);
+					label.setMaxHeight(100);
+					label.setWrapText(true);
+					label.setTextAlignment(TextAlignment.CENTER);
+					
+					node = label;
+				}
+
+				node.setStyle("-fx-border-color: black; -fx-border-style: solid; -fx-border-width: 1px;");
+				GridPane.setHalignment(node, HPos.CENTER);
+
+				node.setOnMouseClicked(e -> {
+					if (selectedProducts.contains(p)) {
+						selectedProducts.remove(p);
+
+						deselectHandler.exec(p);
+
+						node.setStyle("-fx-border-color: black; -fx-border-style: solid; -fx-border-width: 1px;");
+					} else {
+						selectedProducts.add(p);
+
+						selectHandler.exec(p);
+
+						node.setStyle("-fx-border-color: black; -fx-border-style: solid; -fx-border-width: 2px;");
+					}
+
+				});
+
+				pane.add(node, i % 4, i / 4);
 			}
-			
+
 			setCenter(pane);
 		}
 	}
