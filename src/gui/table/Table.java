e1f88e3709d0049548b5c3c245c9c8223c224fba
Mathias Hansen <mathiasosthan@gmail.com>
2017-04-19 09:11:09 +0200

    Table column constraints

--- src/gui/table/Table.java
+++ src/gui/table/Table.java
@@ -6,142 +6,122 @@ import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class Table<A> {
-    private BorderPane pane = new BorderPane(new VBox(), new HBox(), null, null, null);
-    private List<A> items;
-    private final List<Column<A>> columns = new ArrayList<>();
-    private final ValidationHandler validationHanlder;
+	private GridPane pane = new GridPane();
+	private List<A> items = new ArrayList<>();
+	private final List<Column<A>> columns = new ArrayList<>();
+	private final ValidationHandler validationHanlder;
 
-    public Table(ValidationHandler validationHanlder) {
-        this.validationHanlder = validationHanlder;
+	public Table(ValidationHandler validationHanlder) {
+		this.validationHanlder = validationHanlder;
 
-        ((HBox) pane.getTop()).setSpacing(10);
-        ((VBox) pane.getCenter()).setSpacing(10);
-    }
+		pane.setHgap(10);
+		pane.setVgap(10);
+	}
 
-    /**
-     * removes and sets all items in the table
-     */
-    public void setItems(List<A> items) {
-        assert items != null;
-        this.items = items;
+	/**
+	 * removes and sets all items in the table
+	 */
+	public void setItems(List<A> items) {
+		assert items != null;
 
-        ((VBox) pane.getCenter()).getChildren().clear();
+		for (int i = this.items.size() - 1; i >= 0; i--) {
+			removeItem(this.items.get(i));
+		}
 
-        for (A item : items) {
-            addRow(item);
-        }
-    }
+		for (A item : items) {
+			addItem(item);
+		}
+	}
 
-    /**
-     * adds an item to the table
-     * @param item cannot already be in the table
-     */
-    public void addItem(A item) {
-        assert item != null;
-        assert !items.contains(item);
+	/**
+	 * adds an item to the table
+	 * 
+	 * @param item
+	 *            cannot already be in the table
+	 */
+	public void addItem(A item) {
+		assert item != null;
+		assert !items.contains(item);
 
-        this.items.add(item);
+		this.items.add(item);
 
-        addRow(item);
-    }
+		int row = this.items.size();
 
-    /**
-     * removes an item from the table
-     * @param item must be it the table
-     */
-    public void removeItem(A item) {
-        assert item != null;
-        assert items.contains(item);
+		for (int col = 0; col < columns.size(); col++) {
+			Column<A> column = columns.get(col);
+			Node node = column.getNode(item);
 
-        int i = items.indexOf(item);
+			pane.add(node, col, row);
+		}
+	}
 
-        this.items.remove(item);
+	/**
+	 * removes an item from the table
+	 * 
+	 * @param item
+	 *            must be it the table
+	 */
+	public void removeItem(A item) {
+		assert item != null;
+		assert items.contains(item);
 
-        ((VBox) pane.getCenter()).getChildren().remove(i);
-    }
+		int i = items.indexOf(item);
 
-    private void addRow(A item) {
-        VBox rows = (VBox) pane.getCenter();
-        HBox row = new HBox();
+		this.items.remove(item);
 
-        row.setSpacing(10);
+		pane.getChildren().remove(i + columns.size() - 1);
+	}
 
-        for (Column<A> column : columns) {
-            Node node = column.getNode(item);
-            String style = "";
+	/**
+	 * returns the pain containing the table contents
+	 */
+	public GridPane getPane() {
+		return pane;
+	}
 
-            if (column.getMinWidth() != null) {
-                style += "-fx-min-width: " + column.getMinWidth() + "px; ";
-            }
+	/**
+	 * adds a column to the table
+	 * 
+	 * @param column
+	 *            cannot be null
+	 */
+	public void addColumn(Column<A> column) {
+		assert column != null;
+		assert !columns.contains(column);
 
-            if (column.getPrefWidth() != null) {
-                style += "-fx-pref-width: " + column.getPrefWidth() + "px; ";
-            }
+		columns.add(column);
 
-            if (column.getMaxWidth() != null) {
-                style += "-fx-max-width: " + column.getMaxWidth() + "px; ";
-            }
+		if (validationHanlder != null) {
+			column.validationHandler = (error, isValid) -> validationHanlder.onValidate(error, isValid());
+		}
 
-            node.setStyle(style);
+		Label columnName = new Label(column.getName());
 
-//			System.out.println(node.getStyle());
+		ColumnConstraints cc = new ColumnConstraints();
+		if (column.getMinWidth() != null)
+			cc.setMinWidth(column.getMinWidth());
+		if (column.getPrefWidth() != null)
+			cc.setPrefWidth(column.getPrefWidth());
+		if (column.getMaxWidth() != null)
+			cc.setMaxWidth(column.getMaxWidth());
+		pane.getColumnConstraints().add(cc);
+		pane.add(columnName, columns.size() - 1, 0);
+	}
 
-            row.getChildren().add(node);
-        }
+	public boolean isValid() {
+		for (Column<A> c : columns) {
+			if (c.isValid() == false) {
+				return false;
+			}
+		}
 
-        rows.getChildren().add(row);
-    }
-
-    /**
-     * returns the pain containing the table contents
-     */
-    public BorderPane getPane() {
-        return pane;
-    }
-
-    /**
-     * adds a column to the table
-     * @param column cannot be null
-     */
-    public void addColumn(Column<A> column) {
-        assert column != null;
-
-        if (validationHanlder != null) {
-            column.validationHandler =
-                (error, isValid) -> validationHanlder.onValidate(error, isValid());
-        }
-
-        Label columnName = new Label(column.getName());
-
-        if (column.getMinWidth() != null) {
-            columnName.setMinWidth(column.getMinWidth());
-        }
-
-        if (column.getPrefWidth() != null) {
-            columnName.setPrefWidth(column.getPrefWidth());
-        }
-
-        if (column.getMaxWidth() != null) {
-            columnName.setMaxWidth(column.getMaxWidth());
-        }
-
-        ((HBox) pane.getTop()).getChildren().add(columnName);
-
-        columns.add(column);
-    }
-
-    public boolean isValid() {
-        for (Column<A> c : columns) {
-            if (c.isValid() == false) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+		return true;
+	}
 
 }
